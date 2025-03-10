345b075e1cd809e2a920bccf76a770d601fdd994
RutvikGujarati <mrrutvikgujarati0007@gmail.com>
2025-03-10 14:56:22 +0530

    added swap of rieva

--- scripts/AuctionDeployer.s.sol
+++ scripts/AuctionDeployer.s.sol
@@ -2,21 +2,21 @@
 pragma solidity ^0.8.20;
 
 import "forge-std/Script.sol";
-import {Ratio_Swapping_Auctions_V1_1} from "../src/AuctionSwap/OneDollarSwap.sol";
+import {Ratio_Swapping_Auctions_V1_1} from "../src/AuctionSwap/RievaSwap.sol";
 contract AuctionSwapDeploy is Script {
     function run() external {
         address davToken = 0x78f8Aba822259d8B5E4E381bc536816874Aa5D86;
         address StatToken = 0xc359f56c63A8117C83AbE84F4BB78d9eF124b567;
-        address OneDollar = 0x4f665Ef2EF5336D26a6c06525DD812786E5614c6;
+        address Rieva = 0xc6d64985E00BB43d93582866bB7eAfc75692f0d8;
         address governance = 0xB511110f312a4C6C4a240b2fE94de55D600Df7a9;
 
-        address pairAddress = 0x6916be7B7a36D8BC1C09EAE5487E92fF837626bB;  // for OneDollar
+        address pairAddress = 0x3c504c7d2a99e212c186Aa0bc47A9e94DD7AC827;
         vm.startBroadcast();
 
         Ratio_Swapping_Auctions_V1_1 swap = new Ratio_Swapping_Auctions_V1_1(
             StatToken,
             davToken,
-            OneDollar,
+            Rieva,
             governance,
             pairAddress
         );
