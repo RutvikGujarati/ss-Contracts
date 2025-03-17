98c59e131b9ed1783232d921fafbd74829d6ef7c
RutvikGujarati <mrrutvikgujarati0007@gmail.com>
2025-03-17 10:11:55 +0530

    10$ swap contract added

--- scripts/AuctionDeployer.s.sol
+++ scripts/AuctionDeployer.s.sol
@@ -2,21 +2,21 @@
 pragma solidity ^0.8.20;
 
 import "forge-std/Script.sol";
-import {Ratio_Swapping_Auctions_V1_1} from "../src/AuctionSwap/DomusSwap.sol";
+import {Ratio_Swapping_Auctions_V1_1} from "../src/AuctionSwap/TenDollarSwap.sol";
 contract AuctionSwapDeploy is Script {
     function run() external {
         address davToken = 0x78f8Aba822259d8B5E4E381bc536816874Aa5D86;
         address StatToken = 0xc359f56c63A8117C83AbE84F4BB78d9eF124b567;
-        address Domus = 0x82627374E1fe45A6918f21e52B4776E3B8c6420b;
+        address tenDollar = 0x916ed6d34d2d26b2173237BB44e0c91bdbBD7222;
         address governance = 0xB511110f312a4C6C4a240b2fE94de55D600Df7a9;
 
-        address pairAddress = 0x7019eE4173420EE652eDC9A26bFfC91469C753db;
+        address pairAddress = 0x86E8330Efe0dFc20Ab8f63DcF95A6a8D66f60C1D;
         vm.startBroadcast();
 
         Ratio_Swapping_Auctions_V1_1 swap = new Ratio_Swapping_Auctions_V1_1(
             StatToken,
             davToken,
-            Domus,
+            tenDollar,
             governance,
             pairAddress
         );
@@ -24,3 +24,4 @@ contract AuctionSwapDeploy is Script {
         vm.stopBroadcast();
     }
 }
+	
