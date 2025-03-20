cc966bd3933b33956e896fdae700fccee2fd92ee
RutvikGujarati <mrrutvikgujarati0007@gmail.com>
2025-03-20 13:40:28 +0530

    currus swap

--- scripts/AuctionDeployer.s.sol
+++ scripts/AuctionDeployer.s.sol
@@ -2,21 +2,22 @@
 pragma solidity ^0.8.20;
 
 import "forge-std/Script.sol";
-import {Ratio_Swapping_Auctions_V1_1} from "../src/AuctionSwap/TenDollarSwap.sol";
+import {Ratio_Swapping_Auctions_V1_1} from "../src/AuctionSwap/CurrusSwap.sol";
 contract AuctionSwapDeploy is Script {
     function run() external {
         address davToken = 0x78f8Aba822259d8B5E4E381bc536816874Aa5D86;
         address StatToken = 0xc359f56c63A8117C83AbE84F4BB78d9eF124b567;
-        address tenDollar = 0x916ed6d34d2d26b2173237BB44e0c91bdbBD7222;
+        address Currus = 0x2c83101586072e2da8490Aed0d49FB192f9e897e;
         address governance = 0xB511110f312a4C6C4a240b2fE94de55D600Df7a9;
 
-        address pairAddress = 0x86E8330Efe0dFc20Ab8f63DcF95A6a8D66f60C1D;
+        address pairAddress = 0xE4a02db896cEE9Dbf32d730dC9874EB058F0ca3f;
+
         vm.startBroadcast();
 
         Ratio_Swapping_Auctions_V1_1 swap = new Ratio_Swapping_Auctions_V1_1(
             StatToken,
             davToken,
-            tenDollar,
+            Currus,
             governance,
             pairAddress
         );
@@ -24,4 +25,3 @@ contract AuctionSwapDeploy is Script {
         vm.stopBroadcast();
     }
 }
-	
