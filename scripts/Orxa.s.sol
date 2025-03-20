920b296d8e7960b6628ec1c626eb8a1d40da1bad
RutvikGujarati <mrrutvikgujarati0007@gmail.com>
2025-03-20 09:26:22 +0530

    added currus

--- scripts/Orxa.s.sol
+++ scripts/Orxa.s.sol
@@ -2,17 +2,17 @@
 pragma solidity ^0.8.20;
 
 import "forge-std/Script.sol";
-import "../src/Tokens/TenDollar.sol";
+import "../src/Tokens/Currus.sol";
 
-contract DeployTenDollar is Script {
+contract DeployCurrus is Script {
     function run() external {
         address davToken = 0x78f8Aba822259d8B5E4E381bc536816874Aa5D86;
         address governance = 0xB511110f312a4C6C4a240b2fE94de55D600Df7a9;
         vm.startBroadcast();
 
-        $10 tenDollar = new $10(davToken, "10$", "10$", governance);
+        Currus currus = new Currus(davToken, "Currus", "Currus", governance);
 
-        console.log("rievaollar deployed at:", address(tenDollar));
+        console.log("rievaollar deployed at:", address(currus));
 
         vm.stopBroadcast();
     }
