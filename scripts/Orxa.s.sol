0d492c067f383cecba502c671bcb1c4d01772619
RutvikGujarati <mrrutvikgujarati0007@gmail.com>
2025-03-25 17:04:36 +0530

    added valir

--- scripts/Orxa.s.sol
+++ scripts/Orxa.s.sol
@@ -2,17 +2,17 @@
 pragma solidity ^0.8.20;
 
 import "forge-std/Script.sol";
-import "../src/Tokens/Currus.sol";
+import "../src/Tokens/Valir.sol";
 
-contract DeployCurrus is Script {
+contract DeployValir is Script {
     function run() external {
         address davToken = 0x78f8Aba822259d8B5E4E381bc536816874Aa5D86;
         address governance = 0xB511110f312a4C6C4a240b2fE94de55D600Df7a9;
         vm.startBroadcast();
 
-        Currus currus = new Currus(davToken, "Currus", "Currus", governance);
+        Valir valir = new Valir(davToken, "Valir", "Valir", governance);
 
-        console.log("rievaollar deployed at:", address(currus));
+        console.log("rievaollar deployed at:", address(valir));
 
         vm.stopBroadcast();
     }
