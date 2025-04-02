622e9265dcf8d2fe3e4e721db2f0d26925e491bd
RutvikGujarati <mrrutvikgujarati0007@gmail.com>
2025-04-02 11:51:58 +0530

    added sanitas Swap

--- scripts/AuctionDeployer.s.sol
+++ scripts/AuctionDeployer.s.sol
@@ -2,22 +2,22 @@
 pragma solidity ^0.8.20;
 
 import "forge-std/Script.sol";
-import {Ratio_Swapping_Auctions_V1_1} from "../src/AuctionSwap/ValirSwap.sol";
+import {Ratio_Swapping_Auctions_V1_1} from "../src/AuctionSwap/SanitasSwap.sol";
 contract AuctionSwapDeploy is Script {
     function run() external {
         address davToken = 0x78f8Aba822259d8B5E4E381bc536816874Aa5D86;
         address StatToken = 0xc359f56c63A8117C83AbE84F4BB78d9eF124b567;
-        address valir = 0x61fB10Ac14b5fe7499A6858A9cF0d80cc1d2fD75;
+        address sanitas = 0xbaB8540DeE05ba25CEc588CE5124aa50b1D7d425;
         address governance = 0xB511110f312a4C6C4a240b2fE94de55D600Df7a9;
 
-        address pairAddress = 0x1d3796D78FfDca93B501Ff442Aba4123BB334Cd1;
+        address pairAddress = 0x1A60E1CA8732634392eb89e68A6c50ea457872c6;
 
         vm.startBroadcast();
 
         Ratio_Swapping_Auctions_V1_1 swap = new Ratio_Swapping_Auctions_V1_1(
             StatToken,
             davToken,
-            valir,
+            sanitas,
             governance,
             pairAddress
         );
