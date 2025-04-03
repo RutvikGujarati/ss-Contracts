b4f31b8717914d63f4bfdbfd4c4f5b6066d989ff
RutvikGujarati <mrrutvikgujarati0007@gmail.com>
2025-04-03 23:47:35 +0530

    Teech contract

--- scripts/Orxa.s.sol
+++ scripts/Orxa.s.sol
@@ -2,17 +2,17 @@
 pragma solidity ^0.8.20;
 
 import "forge-std/Script.sol";
-import "../src/Tokens/Sanitas.sol";
+import "../src/Tokens/Teech.sol";
 
-contract DeploySanitas is Script {
+contract DeployTeech is Script {
     function run() external {
         address davToken = 0x78f8Aba822259d8B5E4E381bc536816874Aa5D86;
         address governance = 0xB511110f312a4C6C4a240b2fE94de55D600Df7a9;
         vm.startBroadcast();
 
-        Sanitas sanitas = new Sanitas(davToken, "Sanitas", "Sanitas", governance);
+        Teech teech = new Teech(davToken, "Teech", "Teech", governance);
 
-        console.log("rievaollar deployed at:", address(sanitas));
+        console.log("rievaollar deployed at:", address(teech));
 
         vm.stopBroadcast();
     }
