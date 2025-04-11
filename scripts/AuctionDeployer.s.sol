949b2a8eec7c2d29a2ff0a800502c4a7e2ae252b
RutvikGujarati <mrrutvikgujarati0007@gmail.com>
2025-04-11 09:06:49 +0530

    added teeah swap

--- scripts/AuctionDeployer.s.sol
+++ scripts/AuctionDeployer.s.sol
@@ -2,22 +2,22 @@
 pragma solidity ^0.8.20;
 
 import "forge-std/Script.sol";
-import {Ratio_Swapping_Auctions_V1_1} from "../src/AuctionSwap/SanitasSwap.sol";
+import {Ratio_Swapping_Auctions_V1_1} from "../src/AuctionSwap/TeeahSwap.sol";
 contract AuctionSwapDeploy is Script {
     function run() external {
         address davToken = 0x78f8Aba822259d8B5E4E381bc536816874Aa5D86;
         address StatToken = 0xc359f56c63A8117C83AbE84F4BB78d9eF124b567;
-        address sanitas = 0xbaB8540DeE05ba25CEc588CE5124aa50b1D7d425;
+        address Teeah = 0x3288DCdcc0c8Fea3Cc84D463352d1DB7B22FCf1c;
         address governance = 0xB511110f312a4C6C4a240b2fE94de55D600Df7a9;
 
-        address pairAddress = 0x1A60E1CA8732634392eb89e68A6c50ea457872c6;
+        address pairAddress = 0xebeE32fAe4fCB913fA25aE16D741BA197510a575;
 
         vm.startBroadcast();
 
         Ratio_Swapping_Auctions_V1_1 swap = new Ratio_Swapping_Auctions_V1_1(
             StatToken,
             davToken,
-            sanitas,
+            Teeah,
             governance,
             pairAddress
         );
